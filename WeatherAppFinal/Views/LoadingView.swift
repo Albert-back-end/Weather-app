7f65a9e21f7f1d2f67f48c24229d6c09022fe9a8
Albert <albert231207@mail.ru>
2024-11-05 21:39:05 +0300

    Add main code to app

---
+++ WeatherAppFinal/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  WeatherAppFinal
+//
+//  Created by Albert on 11/5/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ WeatherAppFinal/WeatherAppFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppFinalApp.swift
+//  WeatherAppFinal
+//
+//  Created by Albert on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
